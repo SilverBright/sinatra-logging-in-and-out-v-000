b1c2df536550adeafa3b63d7ed1044fe55d966e9
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-23 00:18:57 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,4 +1,5 @@
 class Helpers
+
   def self.current_user(session_hash)
     @user = User.find(session_hash[:user_id])
   end
@@ -6,4 +7,5 @@ class Helpers
   def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
   end
+  
 end
